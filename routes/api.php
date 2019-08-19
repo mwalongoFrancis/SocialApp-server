2ea2c7e0900bc34ecc1e4b49b82ba4e7bf1e8071
Francis <ayman@Dancels-MacBook-Pro.local>
2019-08-19 16:34:26 +0300

    api created successfully

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
-
+use App\Http\Controllers\AlbumController;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Route;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -12,7 +12,17 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('albums', ['uses' => 'AlbumController@getAlbums']);
+Route::get('authors', ['uses' => 'AuthorController@getAuthors']);
+Route::post('author', ['uses' => 'AuthorController@postAuthor']);
+Route::post('album/{authorId}', ['uses' => 'AlbumController@postAlbum']);
+Route::get('album/{albumId}', ['uses' => 'AlbumController@getAlbum']);
+Route::put('album/{albumId}', ['uses' => 'AlbumController@putAlbum']);
+Route::delete('album/{albumId}', ['uses' => 'AlbumController@deleteAlbum']);
+Route::get('author/{authorId}', ['uses' => 'AuthorController@getAuthor']);
+Route::put('author/{authorId}', ['uses' => 'AuthorController@putAuthor']);
+Route::delete('author/{authorId}', ['uses' => 'AuthorController@deleteAuthor']);
+
